9862034faa7e3ddb518a13dc9e0b0b7072784c50
Balakumaran <balakumaran247@gmail.com>
2024-12-10 16:53:45 +0530

    added: compute_year_step method

--- core/gee.py
+++ core/gee.py
@@ -12,6 +12,10 @@ ee.Initialize()
 
 class ImageCollections:
     TIMESTAMP_LABEL = 'system:time_start'
+    CALENDAR_START_MONTH = 1
+    CALENDAR_END_MONTH = 12
+    HYDROLOGICAL_START_MONTH = 6
+    HYDROLOGICAL_END_MONTH = 5
 
     def __init__(self, parameter: str) -> None:
         """
@@ -35,16 +39,16 @@ class ImageCollections:
         Returns:
             datetime: The calculated date.
         """
-        print(ee.Date(start_date).format('y-M-d').getInfo())
         end_date = start_date.advance(advance_count, 'month')
-        print(ee.Date(end_date).format('y-M-d').getInfo())
         filtered = asset.filterDate(start_date.advance(
             abs(advance_count)*-1, 'month'), start_date.advance(abs(advance_count), 'month'))
-        print(filtered.size().getInfo())
         adv_filtered = asset.filterDate(
             start_date, end_date) if advance_count > 0 else asset.filterDate(end_date, start_date)
         if adv_filtered.size().getInfo():
             return ImageCollections._get_date(asset, end_date, advance_count)
+        if not filtered.size().getInfo():
+            raise ValueError(f'get_date method failed on {asset.get(
+                'system:id').getInfo()} for {start_date.format().getInfo()}')
         date = filtered.aggregate_max(ImageCollections.TIMESTAMP_LABEL).getInfo(
         ) if advance_count > 0 else filtered.aggregate_min(ImageCollections.TIMESTAMP_LABEL).getInfo()
         return datetime.fromtimestamp(date/1000.0)
@@ -67,13 +71,11 @@ class ImageCollections:
         Returns:
             dict: A dictionary containing the label and the fetched properties.
         """
-        if label == 'last_update':
-            return {label: data}
         asset = ee.ImageCollection(data.get('id'))
-        current_start_date = ee.Date.fromYMD(int(data.get('start_year')), int(
-            data.get('start_month')), int(data.get('start_day')))
-        current_end_date = ee.Date.fromYMD(int(data.get('end_year')), int(
-            data.get('end_month')), int(data.get('end_day')))
+        current_start_date = ee.Date.fromYMD(
+            data.get('start_year'), data.get('start_month'), data.get('start_day'))
+        current_end_date = ee.Date.fromYMD(data.get('end_year'),
+                                           data.get('end_month'), data.get('end_day'))
         with ThreadPoolExecutor() as ex:
             futures = [
                 ex.submit(ImageCollections._get_date,
@@ -84,14 +86,38 @@ class ImageCollections:
         min_date = futures[0].result()
         max_date = futures[1].result()
         dates_dict = {
-            'start_year': f'{min_date:%Y}',
-            'start_month': f'{min_date:%m}',
-            'start_day': f'{min_date:%d}',
-            'end_year': f'{max_date:%Y}',
-            'end_month': f'{max_date:%m}',
-            'end_day': f'{max_date:%d}'
+            'start_year': int(f'{min_date:%Y}'),
+            'start_month': int(f'{min_date:%m}'),
+            'start_day': int(f'{min_date:%d}'),
+            'end_year': int(f'{max_date:%Y}'),
+            'end_month': int(f'{max_date:%m}'),
+            'end_day': int(f'{max_date:%d}')
         }
         return {label: data | dates_dict}
+
+    @staticmethod
+    def _compute_year_step(data: dict) -> dict:
+        """
+        Computes the adjusted start and end years for calendar and hydrological years based on the given data.
+        Args:
+            data (dict): A dictionary where keys are labels and values are dictionaries containing 
+                         'start_month', 'end_month', 'start_year', and 'end_year'.
+        Returns:
+            dict: A dictionary with the same keys as the input, but with values updated to include 
+                  'calendar_start', 'hydrological_start', 'calendar_end', and 'hydrological_end' years.
+        """
+        for label, properties in data.items():
+            start_month = properties.get('start_month')
+            end_month = properties.get('end_month')
+            start_year = properties.get('start_year')
+            end_year = properties.get('end_year')
+            data[label] |= {
+                'calendar_start': start_year if start_month == ImageCollections.CALENDAR_START_MONTH else start_year + 1,
+                'hydrological_start': start_year if start_month < ImageCollections.HYDROLOGICAL_START_MONTH else start_year + 1,
+                'calendar_end': end_year if end_month == ImageCollections.CALENDAR_END_MONTH else end_year - 1,
+                'hydrological_end': end_year - 1 if end_month > ImageCollections.HYDROLOGICAL_END_MONTH else end_year - 2
+            }
+        return data
 
     @staticmethod
     def update_metadata(date: str) -> None:
@@ -113,6 +139,7 @@ class ImageCollections:
             if not last_update:
                 raise KeyError(
                     'last_update key not found in the imagecollections JSON')
+            data.pop('last_update', None)
             out_dict = {}
             with ThreadPoolExecutor() as ex:
                 futures = [ex.submit(ImageCollections._fetch_properties, k, v)
@@ -120,6 +147,7 @@ class ImageCollections:
             for future in as_completed(futures):
                 out = future.result()
                 out_dict |= out
+            out_dict = ImageCollections._compute_year_step(out_dict)
             out_dict['last_update'] = date
             write_json(out_dict)
 
