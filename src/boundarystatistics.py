7bb144525b0a3b0fc6013c5a77d9ad8032dfb2f3
Balakumaran <balakumaran247@gmail.com>
2024-12-13 15:19:37 +0530

    updated: UI elements results aggregated

--- src/boundarystatistics.py
+++ src/boundarystatistics.py
@@ -1,165 +1,51 @@
-from PyQt5.QtCore import QCoreApplication
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import (QWidget, QListWidget, QListWidgetItem,
-                                QLabel, QLineEdit, QGridLayout, QMenu,
-                                QAction, QCheckBox, QComboBox, QSpinBox,
-                                QFileDialog, QPushButton, QRadioButton,
-                                QButtonGroup)
-import os, inspect
-from processing.gui.wrappers import WidgetWrapper
-from qgis.core import (QgsProcessing,
-                        QgsProcessingAlgorithm,
-                        QgsProcessingParameterFeatureSource,
-                        QgsProcessingParameterFileDestination,
-                        QgsProcessingParameterEnum,
-                        QgsProcessingParameterNumber,
-                        QgsProcessingParameterString,
-                        QgsProcessingException,
-                        QgsProcessingParameterVectorLayer,
-                        QgsProcessingOutputFile,
-                        QgsProcessingParameterMatrix,
-                        QgsMapLayerProxyModel)
-from qgis.gui import (QgsMapLayerComboBox, QgsFieldComboBox, QgsExternalStorageFileWidget)
+import inspect
+import os
 from datetime import datetime
+
+from processing.gui.wrappers import WidgetWrapper
+from PyQt5.QtCore import QCoreApplication
+from qgis.core import (QgsMapLayerProxyModel, QgsProcessing,
+                       QgsProcessingAlgorithm, QgsProcessingException,
+                       QgsProcessingOutputFile, QgsProcessingParameterEnum,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFileDestination,
+                       QgsProcessingParameterMatrix,
+                       QgsProcessingParameterNumber,
+                       QgsProcessingParameterString,
+                       QgsProcessingParameterVectorLayer)
+from qgis.gui import (QgsExternalStorageFileWidget, QgsFieldComboBox,
+                      QgsMapLayerComboBox)
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import (QAction, QButtonGroup, QCheckBox, QComboBox,
+                                 QFileDialog, QGridLayout, QLabel, QLineEdit,
+                                 QListWidget, QListWidgetItem, QMenu,
+                                 QPushButton, QRadioButton, QSpinBox, QWidget)
 
 # https://gis.stackexchange.com/questions/465952/how-to-chose-a-vector-layer-chose-a-field-then-chose-values-using-parameterase
 
 
 class BoundaryStatsAlgorithm(QgsProcessingAlgorithm):
     INPUT_PARAMS = 'INPUT_PARAMS'
-    
-    # OUTPUT = 'OUTPUT NAME'
-    # INPUT = 'INPUT LAYER'
-    # START_YEAR = 'STARTING YEAR'
-    # END_YEAR = 'ENDING YEAR'
-    # SPAN = 'SPAN'
-    # PARAMETER = 'PARAMETER'
-    # SPATIALSTAT = 'SPATIAL REDUCER'
-    # TEMPORALSTAT = 'TEMPORAL REDUCER'
-    # TEMPORALSTEP = 'TEMPORAL STEP'
-    # COLNAME = 'UNIQUE FIELD'
 
-    # STEPOPTIONS = ['Monthly','Yearly']
-    # SPANOPTIONS = ['Calendar Year','Hydrological Year']
-    # REDUCERS = ['Mean', 'Median', 'Max', 'Min', 'Mode', 'Total']
-    # DATASETS = ['IMD Rainfall',
-    #         'IMD Max Temperature',
-    #         'IMD Min Temperature',
-    #         'ETa SSEBop'
-    #             ]
-    
     def initAlgorithm(self, config=None):
-        param = QgsProcessingParameterMatrix(self.INPUT_PARAMS, 'Boundary Statistics')
+        param = QgsProcessingParameterMatrix(
+            self.INPUT_PARAMS, 'Boundary Statistics')
         param.setMetadata({'widget_wrapper': {'class': BoundaryStatsWidget}})
         self.addParameter(param)
-        
+
     def processAlgorithm(self, parameters, context, feedback):
-        user_options = self.parameterAsMatrix(parameters, self.INPUT_PARAMS, context)
-        input_vector_layer, selected_features, unique_field = user_options
+        user_options = self.parameterAsMatrix(
+            parameters, self.INPUT_PARAMS, context)
+        keys = ('INPUT_LAYER', 'SELECTED_FEATURES', 'INPUT_FIELD', 'PARAMETER', 'SPAN',
+                'TEMPORALSTEP', 'START_YEAR', 'END_YEAR', 'SPATIALSTAT', 'TEMPORALSTAT',
+                'SCALE', 'TILESCALE', 'EXPORT_TO', 'EXPORT_PATH')
+        kwargs = dict(zip(keys, user_options))
 
         from ..core.gee import ImageCollections
-        ImageCollections.update_metadata(f'{datetime.now():%Y-%m-%d}', feedback)
-        
-        return {'INPUT_LAYER': input_vector_layer,
-                'SELECTED_FEATURES': selected_features,
-                'INPUT_FIELD': unique_field}
-    
-    # def initAlgorithm(self, config=None):
-    #     # input of Polygon Layer
-    #     self.addParameter(
-    #         QgsProcessingParameterFeatureSource(
-    #             self.INPUT,
-    #             self.tr(self.INPUT.title()),
-    #             [QgsProcessing.TypeVectorPolygon, QgsProcessing.TypeVectorPoint]
-    #         )
-    #     )
+        ImageCollections.update_metadata(
+            f'{datetime.now():%Y-%m-%d}', feedback)
 
-    #     self.addParameter(
-    #         QgsProcessingParameterNumber(
-    #             self.START_YEAR,
-    #             self.tr(self.START_YEAR.title()),
-    #             type=QgsProcessingParameterNumber.Integer,
-    #             defaultValue=2020,
-    #             optional=False,
-    #             minValue=1994)
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterNumber(
-    #             self.END_YEAR,
-    #             self.tr(self.END_YEAR.title()),
-    #             type=QgsProcessingParameterNumber.Integer,
-    #             defaultValue=2020,
-    #             optional=False,
-    #             minValue=1994)
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterEnum(
-    #             self.SPAN,
-    #             self.tr(self.SPAN.title()),
-    #             options=[self.tr(item) for item in self.SPANOPTIONS],
-    #             defaultValue=0,
-    #             optional=False
-    #         )
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterEnum(
-    #             self.PARAMETER,
-    #             self.tr(self.PARAMETER.title()),
-    #             options=[self.tr(item) for item in self.DATASETS],
-    #             defaultValue=0,
-    #             optional=False
-    #         )
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterEnum(
-    #             self.TEMPORALSTEP,
-    #             self.tr(self.TEMPORALSTEP.title()),
-    #             options = [self.tr(item) for item in self.STEPOPTIONS],
-    #             defaultValue = 0,
-    #             optional = False
-    #         )
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterEnum(
-    #             self.TEMPORALSTAT,
-    #             self.tr(self.TEMPORALSTAT.title()),
-    #             options = [self.tr(item) for item in self.REDUCERS],
-    #             defaultValue = 0,
-    #             optional = False
-    #         )
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterEnum(
-    #             self.SPATIALSTAT,
-    #             self.tr(self.SPATIALSTAT.title()),
-    #             options=[self.tr(item) for item in self.REDUCERS],
-    #             defaultValue = 0,
-    #             optional = False
-    #         )
-    #     )
-
-    #     self.addParameter(
-    #         QgsProcessingParameterString(
-    #             self.COLNAME,
-    #             self.tr(self.COLNAME.title()),
-    #             optional = False
-    #         )
-    #     )
-
-    #     # file output of type CSV.
-    #     self.addParameter(
-    #         QgsProcessingParameterFileDestination(
-    #             self.OUTPUT,
-    #             self.tr(self.OUTPUT.title()),
-    #             'CSV files (*.csv)',
-    #         )
-    #     )
+        return kwargs
 
     # def set_feedback(self, feedback, perc):
     #     if feedback.isCanceled():
@@ -183,13 +69,13 @@ class BoundaryStatsAlgorithm(QgsProcessingAlgorithm):
     #     temp_reducer = self.REDUCERS[temp_reducer]
     #     spat_reducer = self.REDUCERS[spat_reducer]
     #     param = self.DATASETS[param]
-        
+
     #     if col_name not in [field.name() for field in source_lyr.fields()]:
     #         raise QgsProcessingException('Unique Field Heading not available.')
-        
+
     #     from .boundarystatisticscore import BoundaryWiseStats
     #     from ..utils import get_feature_collection
-        
+
     #     self.set_feedback(feedback,1)
     #     f_col = get_feature_collection(source_lyr)
     #     self.set_feedback(feedback,15)
@@ -235,6 +121,7 @@ class BoundaryStatsAlgorithm(QgsProcessingAlgorithm):
     def createInstance(self):
         return BoundaryStatsAlgorithm()
 
+
 class BoundaryStatsWidget(WidgetWrapper):
 
     def createWidget(self):
@@ -242,12 +129,27 @@ class BoundaryStatsWidget(WidgetWrapper):
         return self.custom_widget
 
     def value(self):
-        source_layer = self.custom_widget.get_layer()
+        source_layer = self.custom_widget.lyr_cb.currentLayer()  # get_layer()
         selected_features = self.custom_widget.onlyselected_cb.isChecked()
-        source_field = self.custom_widget.get_field()
-        params = [source_layer, selected_features, source_field]
-        return params
-    
+        source_field = self.custom_widget.fld_cb.currentField()  # get_field()
+        parameter = self.custom_widget.parm_cb.currentText()
+        span = self.custom_widget.span_cb.currentText()
+        step = self.custom_widget.step_cb.currentText()
+        start_year = self.custom_widget.start_year_int.value()
+        end_year = self.custom_widget.end_year_int.value()
+        spatial_reducer = self.custom_widget.spatial_cb.currentText()
+        temporal_reducer = self.custom_widget.temporal_cb.currentText()
+        scale = self.custom_widget.scale_int.value()
+        tilescale = self.custom_widget.tilescale_cb.currentText()
+        export_to = self.custom_widget.export_option
+        export_path = self.custom_widget.export_ln.text()
+        return [
+            source_layer, selected_features, source_field, parameter, span, step,
+            start_year, end_year, spatial_reducer, temporal_reducer, scale,
+            tilescale, export_to, export_path
+        ]
+
+
 class customParametersWidget(QWidget):
     PARAMETERS = [
         'IMD Rainfall',
@@ -255,37 +157,37 @@ class customParametersWidget(QWidget):
         'IMD Min Temperature',
         'ETa SSEBop'
     ]
-    SPANOPTIONS = ['Calendar Year','Hydrological Year']
-    STEPOPTIONS = ['Monthly','Yearly']
+    SPANOPTIONS = ['Calendar Year', 'Hydrological Year']
+    STEPOPTIONS = ['Monthly', 'Yearly']
     REDUCERS = ['Mean', 'Median', 'Max', 'Min', 'Mode', 'Total']
     TILESCALE = ["1", "2", "4"]
+
     def __init__(self):
         super(customParametersWidget, self).__init__()
-        
+
         self.lyr_lbl = QLabel('Input Vector Layer:')
         self.lyr_cb = QgsMapLayerComboBox(self)
         self.lyr_cb.setFilters(QgsMapLayerProxyModel.VectorLayer)
         self.lyr_cb.layerChanged.connect(self.layer_changed)
 
         self.onlyselected_cb = QCheckBox('Only Selected Features', self)
-        
+
         self.parm_lbl = QLabel('Select Parameter:')
         self.parm_cb = QComboBox(self)
         self.parm_cb.addItems(self.PARAMETERS)
-        
+
         self.span_lb1 = QLabel('Select Span:')
         self.span_cb = QComboBox(self)
         self.span_cb.addItems(self.SPANOPTIONS)
-        
+
         self.step_lb1 = QLabel('Select Step:')
         self.step_cb = QComboBox(self)
         self.step_cb.addItems(self.STEPOPTIONS)
-        
+
         self.fld_lbl = QLabel('Select Unique Field:')
         self.fld_cb = QgsFieldComboBox(self)
         self.fld_cb.setLayer(self.lyr_cb.currentLayer())
-        # self.fld_cb.fieldChanged.connect(self.populate_list_widget)
-        
+
         self.start_year_lb1 = QLabel('Start Year:')
         self.start_year_int = QSpinBox(self)
         self.start_year_int.setMinimum(1994)
@@ -293,7 +195,7 @@ class customParametersWidget(QWidget):
         self.start_year_int.setValue(2020)
         self.start_year_int.setSingleStep(1)
         self.start_year_int.setWrapping(True)
-        
+
         self.end_year_lb1 = QLabel('End Year:')
         self.end_year_int = QSpinBox(self)
         self.end_year_int.setMinimum(1994)
@@ -301,15 +203,15 @@ class customParametersWidget(QWidget):
         self.end_year_int.setValue(2020)
         self.end_year_int.setSingleStep(1)
         self.end_year_int.setWrapping(True)
-        
+
         self.spatial_lb1 = QLabel('Spatial Reducer:')
         self.spatial_cb = QComboBox(self)
         self.spatial_cb.addItems(self.REDUCERS)
-        
+
         self.temporal_lb1 = QLabel('Temporal Reducer:')
         self.temporal_cb = QComboBox(self)
         self.temporal_cb.addItems(self.REDUCERS)
-        
+
         self.scale_lb1 = QLabel('Scale (optional):')
         self.scale_int = QSpinBox(self)
         self.scale_int.setMinimum(10)
@@ -317,11 +219,11 @@ class customParametersWidget(QWidget):
         self.scale_int.setValue(100)
         self.scale_int.setSingleStep(10)
         self.scale_int.setWrapping(True)
-        
+
         self.tilescale_lb1 = QLabel('tileScale (optional):')
         self.tilescale_cb = QComboBox(self)
         self.tilescale_cb.addItems(self.TILESCALE)
-        
+
         self.export_lb1 = QLabel('Export to:')
         self.rbgroup = QButtonGroup()
         self.export_rb1 = QRadioButton("Local (Light Computation)")
@@ -333,7 +235,7 @@ class customParametersWidget(QWidget):
         self.export_btn = QPushButton('Browse')
         self.export_btn.clicked.connect(self.browse)
         self.export_ln = QLineEdit('Path to export CSV file', self)
-        
+
         self.layout = QGridLayout()
         self.layout.addWidget(self.lyr_lbl, 0, 0, 1, 1)
         self.layout.addWidget(self.lyr_cb, 0, 1, 1, 2)
@@ -363,9 +265,9 @@ class customParametersWidget(QWidget):
         self.layout.addWidget(self.export_rb2, 6, 2, 1, 1)
         self.layout.addWidget(self.export_ln, 7, 0, 1, 3)
         self.layout.addWidget(self.export_btn, 7, 3, 1, 1)
-        
+
         self.setLayout(self.layout)
-    
+
     def export_type(self):
         if self.export_rb1.isChecked():
             self._export_type_behaviour('local', True)
@@ -376,16 +278,11 @@ class customParametersWidget(QWidget):
         self.export_option = arg0
         self.export_ln.setVisible(arg1)
         self.export_btn.setVisible(arg1)
-    
+
     def browse(self):
-        self.export_path = QFileDialog.getSaveFileName(None, self.tr("Save As"),None,self.tr("CSV files (*.csv)"))
+        self.export_path = QFileDialog.getSaveFileName(
+            None, self.tr("Save As"), None, self.tr("CSV files (*.csv)"))
         self.export_ln.setText(self.export_path[0])
-    
+
     def layer_changed(self, lyr):
         self.fld_cb.setLayer(lyr)
-    
-    def get_layer(self):
-        return self.lyr_cb.currentLayer()
-        
-    def get_field(self):
-        return self.fld_cb.currentField()
